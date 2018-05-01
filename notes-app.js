7633c02d6211b83defb59b61958add62f4a5f36a
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-01 15:31:19 -0500

    sample Web page

--- notes-app.js
+++ notes-app.js
@@ -1,12 +1,12 @@
 //DOm -- Document Object Model
 
 // Query and remove single element
-const p =  document.querySelector('p');
-console.log(p);
-p.remove();
+// const p =  document.querySelector('p');
+// console.log(p);
+// p.remove();
 
 // Query and remove all elements
 const ps = document.querySelectorAll('p');
 ps.forEach(function(){
-    p.remove();
+    ps.remove();
 })
